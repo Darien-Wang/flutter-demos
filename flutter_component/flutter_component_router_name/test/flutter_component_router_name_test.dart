5b73540b3b615e7dc739c731d8965e8efb498089
Nannan Wang <Nannan.Wang@partner.bmw.com>
2019-10-25 14:51:34 +0800

    add Unit Test for Flutter package of flutter_component_router_name

--- flutter_component/flutter_component_router_name/test/flutter_component_router_name_test.dart
+++ flutter_component/flutter_component_router_name/test/flutter_component_router_name_test.dart
@@ -1,5 +1,11 @@
+import 'package:flutter_test/flutter_test.dart';
 import 'package:flutter_component_router_name/flutter_component_router_name.dart';
 
 void main() {
-  print(RouteName.FEATURE_A_PAGE_A.toString());
+  group("RouteName", () {
+    print(RouteName.FEATURE_A_PAGE_A.toString());
+    test("contain feature a page a", () {
+      RouteName.values.contains(RouteName.FEATURE_A_PAGE_A);
+    });
+  });
 }
