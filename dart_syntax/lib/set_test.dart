7cfd313fcb858139de6b9d4d4b8f62639f726709
Nannan Wang <Nannan.Wang@partner.bmw.com>
2019-11-18 15:09:08 +0800

    add document

--- dart_syntax/lib/set_test.dart
+++ dart_syntax/lib/set_test.dart
@@ -1,7 +1,20 @@
 // ignore: slash_for_doc_comments
+import 'dart:math';
+
+// ignore: slash_for_doc_comments
 /**
  * Set Test 是为了证明，Set中，重复元素的判定是基于Hashcode和==操作符的，
- * 如果二者都相等，则Set判定为重复
+ * 首先判断hash值，不相等，直接添加
+ * 其次判定==是否返回true，如果为false，认定为不相同元素，进行添加
+ * 最后认定为相同元素，进行添加
+ * 下面是伪代码：
+ * if(a.hashcode != b.hashcode) {
+ *    addData
+ * } else if(a != b) {
+ *    addData
+ * } else {
+ *    return
+ * }
  */
 main(List<String> arguments) {
   var a1 = A();
@@ -15,9 +28,16 @@ main(List<String> arguments) {
 
 class A {
   @override
-  bool operator ==(other) => true;
+  bool operator ==(other) {
+    print('执行了==');
+    return true;
+  }
 
 //
   @override
-  int get hashCode => 1;
+  int get hashCode {
+    print('执行了hashCode');
+    return 1 + Random().nextInt(100);
+  }
+
 }
