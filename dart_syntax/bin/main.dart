4f456dd3e50d10561734e1354190bde60340ce0c
Nannan Wang <Nannan.Wang@partner.bmw.com>
2019-11-22 09:18:18 +0800

    add document for flutter_bloc

--- dart_syntax/bin/main.dart
+++ dart_syntax/bin/main.dart
@@ -2,22 +2,8 @@
 import 'dart:async';
 
 main(List<String> arguments) {
-  getNumbers().forEach((element) {
-    print(element);
-  });
+
 }
-
-Stream<int> getNumbers() async* {
-  yield 0;
-  for (int i = 0; i < 10; i++) {
-    yield i;
-  }
-}
-
-Stream<int> skip1
-(
-)
-
 
 
 
