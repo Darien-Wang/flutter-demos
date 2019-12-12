8ca82f8ec370ce82ec58cccf6f43e2602f787eb4
Nannan Wang <Nannan.Wang@partner.bmw.com>
2019-12-12 12:58:32 +0800

    update dart

--- dart_syntax/bin/main.dart
+++ dart_syntax/bin/main.dart
@@ -1,26 +1,5 @@
 //说明一个普通的函数的闭包能力
 void main() {
-  var a = Widget("aaa");
-  var p = Person();
-  p.voidCallback = a.printName;
 
-  p.voidCallback();
-  a.name = "bbb";
-  p.voidCallback();
 }
 
-typedef VoidCallback = void Function();
-
-class Widget {
-  String name;
-
-  Widget(this.name);
-
-  void printName() {
-    print(name);
-  }
-}
-
-class Person {
-  VoidCallback voidCallback;
-}
