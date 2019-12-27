c5dcb1b38541ca561265cec92124d64ba8590937
nannan.wang <Nannan.Wang@partner.bmw.com>
2019-12-27 10:06:57 +0800

    add flutter_animation demo

--- flutter_framework/lib/main.dart
+++ flutter_framework/lib/main.dart
@@ -3,30 +3,28 @@ import 'package:flutter/material.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: FlatButton(
-              onPressed: () {
-                Navigator.of(context).push(
-                    MaterialPageRoute(builder: (context) => SecondPage()));
-              },
-              child: Text('跳转')),
-        ),
-      ),
+      home: FirstPage(),
     );
   }
+}
 
-  void printIt(BuildContext context) {
-    print('===================');
-    print("context.runtimeType = ${context.runtimeType}");
-    print('runtimeType = $runtimeType');
-    print('toStringShort = ${this.toStringShort()}');
-    print('toString = ${this.toString()}');
-    print('toStringDeep = ${this.toStringDeep()}');
-    print('toStringShallow = ${this.toStringShallow()}');
+class FirstPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: FlatButton(
+            onPressed: () {
+              Navigator.of(context).push(
+                  MaterialPageRoute(builder: (context) => SecondPage()));
+            },
+            child: Text('跳转')),
+      ),
+    );
   }
 }
 
@@ -34,7 +32,14 @@ class SecondPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
+      body: Center(
+        child: FlatButton(
+            onPressed: () {
+              Navigator.of(context).push(
+                  MaterialPageRoute(builder: (context) => SecondPage()));
+            },
+            child: Text('跳转')),
+      ),
     );
   }
 }
