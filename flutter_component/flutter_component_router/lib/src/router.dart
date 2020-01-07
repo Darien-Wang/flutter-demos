bd0f33e5b4cf542df914e783a69a5077718ef7b1
nannan.wang <Nannan.Wang@partner.bmw.com>
2020-01-07 18:47:08 +0800

    add comment

--- flutter_component/flutter_component_router/lib/src/router.dart
+++ flutter_component/flutter_component_router/lib/src/router.dart
@@ -39,13 +39,15 @@ Route routeFactory(RouteSettings settings) {
   //注意这里有一个坑，因为在具体的page里面，执行ConnectedNavigator的相关静态方法是可以标注泛型的，这个泛型也决定了需要使用的Route的泛型
   //但是如果强制指定泛型，只是约束页面的返回值，而且需要对特定的页面routeName全部做校验，所以我认为就用默认的dynamic，不要使用泛型
   //后续考虑去除ConnectedNavigator类方法的泛型。
-  if (_routesNotNeedParameters.containsKey(settings.name)) {
+  if (_routesNotNeedParameters.containsKey(
+      settings.name)) { //不需要参数的路由，并且已经配置了对应的路由名字
     route = CupertinoPageRoute(
         builder: _routesNotNeedParameters[settings.name], settings: settings);
-  } else if (_routesNeedParameters.containsKey(settings.name)) {
+  } else
+  if (_routesNeedParameters.containsKey(settings.name)) { //需要参数的路由，并且配置了对应的名字
     route = CupertinoPageRoute(
         builder: _routesNeedParameters[settings.name], settings: settings);
-  } else {
+  } else { //没有配置的路由
     route = CupertinoPageRoute(
         builder: (context) => UnKnownPage(), settings: settings);
   }
